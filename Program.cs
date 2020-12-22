7ca3ceb9310a2449a4dbffd89d57a46b86dde6da
Deborah Novais <61918962+deborahnovais@users.noreply.github.com>
2020-12-21 22:22:39 -0300

    Concluído

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada de dados
+
+            Console.WriteLine("Soma de dois números");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Digite o primeiro valor:");
+            int valorUm = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor:");
+            int valorDois = int.Parse(Console.ReadLine());
+
+            //Processamento
+
+            int soma = valorUm + valorDois;
+
+            //Exibir
+
+            Console.WriteLine("A soma de " +valorUm+ " + " +valorDois+ " é igual a " +soma);
+        }
+    }
+}
